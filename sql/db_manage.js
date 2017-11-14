86b637ecad35c97736ba88a09a047f287272e0fc
abbas96ali <islab@Just-Another-MacBook-Pro.local>
2017-11-14 20:25:14 +0300

    fixed SQL

--- sql/db_manage.js
+++ sql/db_manage.js
@@ -9,40 +9,40 @@ var db = new sqlite3.Database('./db/qhelp.db', (err) => {
 
 db.serialize(function(){
 
-	// db.run(`CREATE TABLE IF NOT EXISTS customer (
-	// 		email TEXT NOT NULL UNIQUE PRIMARY KEY,
-	// 		password TEXT NOT NULL,
-	// 		first_name TEXT NOT NULL,
-	// 		last_name TEXT NOT NULL,
-	// 		address TEXT NOT NULL,
-	// 		phone_no INTEGER UNIQUE NOT NULL,
-	// 		qid INTEGER UNIQUE NOT NULL,
-	// 		creditcard_no INTEGER UNIQUE,
-	// 		sms BOOLEAN,
-	// 		accountbalance INTEGER NOT NULL)`, [], function(err){
-	// 	if (err){
-	// 		return console.log(err.message);
-	// 	}
-	// 	console.log("customer Table created successfully");
-	// });
+	db.run(`CREATE TABLE IF NOT EXISTS customer (
+			email TEXT NOT NULL UNIQUE PRIMARY KEY,
+			password TEXT NOT NULL,
+			first_name TEXT NOT NULL,
+			last_name TEXT NOT NULL,
+			address TEXT NOT NULL,
+			phone_no INTEGER UNIQUE NOT NULL,
+			qid INTEGER UNIQUE NOT NULL,
+			creditcard_no INTEGER UNIQUE,
+			sms BOOLEAN,
+			accountbalance INTEGER NOT NULL)`, [], function(err){
+		if (err){
+			return console.log(err.message);
+		}
+		console.log("customer Table created successfully");
+	});
 
-	// db.run(`CREATE TABLE IF NOT EXISTS serviceprovider (
-	// 		email TEXT NOT NULL UNIQUE PRIMARY KEY,
-	// 		password TEXT NOT NULL,
-	// 		first_name TEXT NOT NULL,
-	// 		last_name TEXT NOT NULL,
-	// 		address TEXT NOT NULL,
-	// 		phone_no INTEGER UNIQUE NOT NULL,
-	// 		qid INTEGER UNIQUE NOT NULL,
-	// 		creditcard_no INTEGER UNIQUE NOT NULL,
-	// 		businesslicense UNIQUE,
-	// 		accountbalance INTEGER NOT NULL
-  //     )`, [], function(err){
-	// 	if (err){
-	// 		return console.log(err.message);
-	// 	}
-	// 	console.log("serviceprovider Table created successfully");
-	// });
+	db.run(`CREATE TABLE IF NOT EXISTS serviceprovider (
+			email TEXT NOT NULL UNIQUE PRIMARY KEY,
+			password TEXT NOT NULL,
+			first_name TEXT NOT NULL,
+			last_name TEXT NOT NULL,
+			address TEXT NOT NULL,
+			phone_no INTEGER UNIQUE NOT NULL,
+			qid INTEGER UNIQUE NOT NULL,
+			creditcard_no INTEGER UNIQUE NOT NULL,
+			businesslicense UNIQUE,
+			accountbalance INTEGER NOT NULL
+      )`, [], function(err){
+		if (err){
+			return console.log(err.message);
+		}
+		console.log("serviceprovider Table created successfully");
+	});
 
 
   db.run(`CREATE TABLE IF NOT EXISTS category (
@@ -66,11 +66,11 @@ db.serialize(function(){
 
   db.run(`CREATE TABLE IF NOT EXISTS request (
 		req_id INTEGER UNIQUE NOT NULL PRIMARY KEY AUTOINCREMENT,
-		description TEXT UNIQUE NOT NULL,
+		service TEXT UNIQUE NOT NULL,
 		location TEXT NOT NULL,
 		time TEXT NOT NULL,
 		price INTEGER NOT NULL,
-    status INTEGER NOT NULL,
+    req_status INTEGER,
     category_id INTEGER UNIQUE,
     FOREIGN KEY (category_id) REFERENCES category(category_id)
 		)`, [], function(err){
@@ -80,24 +80,25 @@ db.serialize(function(){
 			}
 			console.log("request Table created successfully");
 		});
-  // IN REQ STATUS 0 is pending 1 is accepted 2 is completed 
+  // IN REQ STATUS 0 is pending 1 is accepted 2 is completed 3 is deleted 4 is overdue
 
-	// db.run(`CREATE TABLE IF NOT EXISTS customer_request (
-	// 	req_id INTEGER NOT NULL UNIQUE,
-	// 	email TEXT NOT NULL,
-	// 	FOREIGN KEY (req_id) REFERENCES request(req_id),
-	// 	FOREIGN KEY (email) REFERENCES customer(email))`, [], function(err){
-  //
-	// 		if (err){
-	// 		return console.log(err.message);
-	// 	}
-	// 	console.log("customer_request Table created successfully");
-	// 	});
+	db.run(`CREATE TABLE IF NOT EXISTS customer_request (
+		req_id INTEGER NOT NULL UNIQUE,
+		email TEXT NOT NULL,
+		FOREIGN KEY (req_id) REFERENCES request(req_id),
+		FOREIGN KEY (email) REFERENCES customer(email))`, [], function(err){
+  
+			if (err){
+			return console.log(err.message);
+		}
+		console.log("customer_request Table created successfully");
+		});
 
   db.run(`CREATE TABLE IF NOT EXISTS bid (
     bid_id INTEGER UNIQUE NOT NULL PRIMARY KEY AUTOINCREMENT,
     bid_price INTEGER NOT NULL,
     bid_description TEXT,
+    bid_status INTEGER NOT NULL,
     service_email TEXT NOT NULL,
     FOREIGN KEY (service_email) REFERENCES serviceprovider(email)
     )`, [], function(err){
@@ -183,12 +184,84 @@ db.serialize(function(){
 
 //****************************** DROPPING A TABLE EXAMPLE ****************************************//
 
-	// db.run("DROP TABLE IF EXISTS login", [], function(err){
+	// db.run("DROP TABLE IF EXISTS customer", [], function(err){
 	// 	if (err){
 	// 		return console.log(err.message);
 	// 	}
-	// 	console.log("Table dropped successfully!");
+	// 	console.log("customer Table dropped successfully!");
 	// });
+
+ //  db.run("DROP TABLE IF EXISTS serviceprovider", [], function(err){
+ //    if (err){
+ //      return console.log(err.message);
+ //    }
+ //    console.log("serviecprovider Table dropped successfully!");
+ //  });
+
+ //  db.run("DROP TABLE IF EXISTS category", [], function(err){
+ //    if (err){
+ //      return console.log(err.message);
+ //    }
+ //    console.log("category Table dropped successfully!");
+ //  });
+
+ //  db.run("DROP TABLE IF EXISTS service_category", [], function(err){
+ //    if (err){
+ //      return console.log(err.message);
+ //    }
+ //    console.log("service_category Table dropped successfully!");
+ //  });
+
+  // db.run("DROP TABLE IF EXISTS request", [], function(err){
+  //   if (err){
+  //     return console.log(err.message);
+  //   }
+  //   console.log("request Table dropped successfully!");
+  // });
+
+ //  db.run("DROP TABLE IF EXISTS customer_request", [], function(err){
+ //    if (err){
+ //      return console.log(err.message);
+ //    }
+ //    console.log("customer_request Table dropped successfully!");
+ //  });
+
+ //  db.run("DROP TABLE IF EXISTS bid", [], function(err){
+ //    if (err){
+ //      return console.log(err.message);
+ //    }
+ //    console.log("bid Table dropped successfully!");
+ //  });
+
+ //  db.run("DROP TABLE IF EXISTS bid_service", [], function(err){
+ //    if (err){
+ //      return console.log(err.message);
+ //    }
+ //    console.log("bid_service Table dropped successfully!");
+ //  });
+
+ //  db.run("DROP TABLE IF EXISTS request_bid", [], function(err){
+ //    if (err){
+ //      return console.log(err.message);
+ //    }
+ //    console.log("request_bid Table dropped successfully!");
+ //  });
+
+ //  db.run("DROP TABLE IF EXISTS trans", [], function(err){
+ //    if (err){
+ //      return console.log(err.message);
+ //    }
+ //    console.log("trans Table dropped successfully!");
+ //  });
+
+ //  db.run("DROP TABLE IF EXISTS evaluate", [], function(err){
+ //    if (err){
+ //      return console.log(err.message);
+ //    }
+ //    console.log("evaluate Table dropped successfully!");
+ //  });
+
+
 
 });
 
