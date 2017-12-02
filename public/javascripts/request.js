566673f67efaec0ad6c2fbe259fe14d863e8b627
zakmaruf <zakmaruf008@gmail.com>
2017-12-03 00:01:58 +0300

    header image

--- public/javascripts/request.js
+++ public/javascripts/request.js
@@ -7,7 +7,7 @@ function add_fields() {
     room++;
     var objTo = document.getElementById('additional')
     var divtest = document.createElement("div");
-    divtest.innerHTML = '<div class="col-lg-12 text-center"><div class="service-box mt-5 mx-auto"><div class="row" style="width: 800px;"><h5 class="mb-3" style="padding-left: 30px;padding-top: 5px;padding-right: 32px;">Pickup Location '+room+':</h4><input type = "text" style="width: 330px;" name = "location2" id = "location2" placeholder = "Room 1202 in CMU, Qatar Foundation"></div></div></div>';
+    divtest.innerHTML = '<div class="col-lg-12 text-center"><div class="service-box mt-5 mx-auto"><div class="row" style="width: 800px;"><h5 class="mb-3" style="padding-left: 30px;padding-top: 5px;padding-right: 32px;">Pickup Location:</h4><input type = "text" style="width: 330px;" name = "location2" id = "location2" placeholder = "Room 1202 in CMU, Qatar Foundation"></div></div></div>';
     
     objTo.appendChild(divtest)
 }
@@ -17,7 +17,7 @@ function add_time() {
     roomtime++;
     var objTo = document.getElementById('additionaltime')
     var divtest = document.createElement("div");
-    divtest.innerHTML = '<div class="col-lg-12 text-center"><div class="service-box mt-5 mx-auto"><div class="row" style="width: 800px;"><h5 class="mb-3" style="padding-left: 30px;padding-top: 5px;padding-right: 20px;">Starting Time '+roomtime+':</h4><input type="datetime-local" style="width: 370px;"name = "stime2" id = "stime"  placeholder = "Today by 12pm"></div></div></div>';
+    divtest.innerHTML = '<div class="col-lg-12 text-center"><div class="service-box mt-5 mx-auto"><div class="row" style="width: 800px;"><h5 class="mb-3" style="padding-left: 30px;padding-top: 5px;padding-right: 20px;">Starting Time:</h4><input type="datetime-local" style="width: 370px;"name = "stime2" id = "stime"  placeholder = "Today by 12pm"></div></div></div>';
    
     objTo.appendChild(divtest)
 }
